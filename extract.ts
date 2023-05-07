3550f87c6f073a6506f73045cc249a20e1e3f4b0
UtopiaMinT <9797160+UtopiaMinT@users.noreply.github.com>
2023-05-08 01:56:11 +0800

    feat: extract tx source and destination (for transfers)

--- extract.ts
+++ extract.ts
@@ -4,8 +4,55 @@ import * as dotenv from "dotenv";
 
 const MEMO_PROGRAM_ID = "MemoSq4gqABAXKb96qnH8TysNcWxMyWCqXgDLGmfcHr";
 const TOKEN_PROGRAM_ID = "TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA";
+const ATOKEN_PROGRAM_ID = "ATokenGPvbdGVxr1b2hvZbsiqW5xWH25efTNsLJA8knL";
 const SYSTEM_PROGRAM_ID = "11111111111111111111111111111111";
 const COMPUTE_BUDGET_PROGRAM_ID = "ComputeBudget111111111111111111111111111111";
+
+function extractTarget(details) {
+    let transfers = 0;
+    let signer = details.transaction.message.accountKeys.filter(x => x.signer)[0].pubkey;
+    let isSimpleTransfer = details.transaction.message.instructions.every((x: any) => {
+        // sol-trasnfer
+        if (x.programId === SYSTEM_PROGRAM_ID && x.parsed?.type === "transfer") {
+            ++transfers;
+            return true;
+        }
+        // spl-transfer, transfer/transferChecked
+        if (x.programId === TOKEN_PROGRAM_ID) {
+            if (x.parsed?.type.startsWith("transfer")) {
+                ++transfers;
+            }
+            return true;
+        }
+        // ata ixs
+        if (x.programId === ATOKEN_PROGRAM_ID) return true;
+        // empty/util ix
+        if (x.accounts && x.accounts.length <= 1) return true;
+        // compute budget (doesn't matter)
+        if (x.programId === COMPUTE_BUDGET_PROGRAM_ID || x.programId == MEMO_PROGRAM_ID) return true;
+    });
+    if (!isSimpleTransfer || transfers !== 1) {
+        console.log("not simple transfer");
+        return [signer, ""];
+    }
+    for (let ix of details.transaction.message.instructions) {
+        // sol-trasnfer
+        if (ix.programId === SYSTEM_PROGRAM_ID && ix.parsed?.type === "transfer") {
+            return [ix.parsed.info.source, ix.parsed.info.destination];
+        }
+        // spl-transfer, transfer/transferChecked
+        if (ix.programId === TOKEN_PROGRAM_ID && ix.parsed?.type.startsWith("transfer")) {
+            let dest = ix.parsed.info.destination;
+            let dIdx = details.transaction.message.accountKeys.findIndex(x => x.pubkey === dest);
+            let src = ix.parsed.info.source;
+            let sIdx = details.transaction.message.accountKeys.findIndex(x => x.pubkey === src);
+            let target = details.meta.postTokenBalances.filter(x => x.accountIndex === dIdx)[0].owner;
+            let source = details.meta.preTokenBalances.filter(x => x.accountIndex === sIdx)[0].owner;
+            return [source, target];
+            // return ix;
+        }
+    }
+}
 
 async function main() {
     dotenv.config();
@@ -18,7 +65,7 @@ async function main() {
     });
     db.connect();
     let rows = await new Promise<any[]>((resolve) => {
-        db.query("select id, details from tx_details where prio_fee_lamports<0 or has_memo<0 or is_program_invocation<0 or is_simple_transfer<0 or is_single_effective_ix<0", async (err, rows) => {
+        db.query("select id, details from tx_details where prio_fee_lamports<0 or has_memo<0 or is_program_invocation<0 or is_simple_transfer<0 or is_single_effective_ix<0 or source=''", async (err, rows) => {
             if (err) {
                 throw err;
             }
@@ -43,12 +90,14 @@ async function main() {
             // spl-transfer, transnfer/transferChecked
             if (x.programId === TOKEN_PROGRAM_ID && x.parsed?.type.startsWith("transfer")) return true;
             // compute budget (doesn't matter)
-            if (x.programId === COMPUTE_BUDGET_PROGRAM_ID) return true;
+            if (x.programId === COMPUTE_BUDGET_PROGRAM_ID || x.programId == MEMO_PROGRAM_ID) return true;
         });
         // is single effective ix
         let isSingleEffectiveIx = details.transaction.message.instructions.filter((x: any) => x.programId != COMPUTE_BUDGET_PROGRAM_ID).length === 1;
+        // source and destination
+        let [source, destination] = extractTarget(details);
         await new Promise<void>((resolve) => {
-            db.query("update tx_details set prio_fee_lamports=?, has_memo=?, is_program_invocation=?, is_simple_transfer=?, is_single_effective_ix=? where id=?", [prioFee, hasMemo, isProgramInvocation, isSimpleTransfer, isSingleEffectiveIx, id], (err) => {
+            db.query("update tx_details set prio_fee_lamports=?, has_memo=?, is_program_invocation=?, is_simple_transfer=?, is_single_effective_ix=?, source=?, destination=? where id=?", [prioFee, hasMemo, isProgramInvocation, isSimpleTransfer, isSingleEffectiveIx, source, destination, id], (err) => {
                 if (err) {
                     throw err;
                 }
